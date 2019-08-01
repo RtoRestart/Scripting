2317a078a68e427568be558da1a4837fe92e6a29
RtoRestart <53551387+RtoRestart@users.noreply.github.com>
2019-08-01 01:53:36 -0500

    Add files via upload

---
+++ Des4-01 Tarifa.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculodetarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int salario;
+            double smmlv;
+
+            Console.Write("Ingrese el valor de su salario = ");
+            salario = int.Parse(Console.ReadLine());
+            smmlv = 828116;
+
+            if (salario < 2*smmlv)
+            {
+                Console.WriteLine("Debe de pagar la tarifa A");
+            }
+            else if (salario < 4*smmlv)
+            {
+                Console.WriteLine("Debe de pagar la tarifa B");
+            }
+            else
+            {
+                Console.WriteLine("Debe de pagar la tarifa C");
+            }
+        }
+    }
+}
