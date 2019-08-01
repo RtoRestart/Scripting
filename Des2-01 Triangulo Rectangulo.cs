699163b426db166e18b63fbf3f259d3c2a9ab69d
RtoRestart <53551387+RtoRestart@users.noreply.github.com>
2019-08-01 01:52:49 -0500

    Add files via upload

---
+++ Des2-01 Triangulo Rectangulo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Des2_01_TrianguloRectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t, a;
+
+            double c, y, z;
+
+            Console.WriteLine("Ingrese los siguientes datos correspondientes al triangulo rectangulo que se desea evaluar");
+            Console.Write("t = ");
+            t = int.Parse(Console.ReadLine());
+            Console.Write("a° = ");
+            a = int.Parse(Console.ReadLine());
+
+            y = t * Math.Sin(a);
+            z = t * Math.Cos(a);
+            c = Math.Atan2(y,z);
+            
+
+            //Se pasa de radianes a grado los angulos
+
+            c = (c * 180) / Math.PI;
+
+            Console.WriteLine("El valor del angulo C es = " + c);
+            Console.WriteLine("El valor del cateto Y = " + y);
+            Console.WriteLine("El valor del cateto Z es = " + z);
+
+        }
+    }
+}
